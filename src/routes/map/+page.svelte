bb58f148c54d0d72e3ab5164edddc0200375b9a0
ptsdpatient <119649887+ptsdpatient@users.noreply.github.com>
2024-02-20 12:15:03 +0530

    map update

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,28 +1,27 @@
 
 <script>
-    let zoomed = false;
-    import map from '$lib/assets/map.png?url'
-    const toggleZoom=()=>window.alert("hello world")
-     
-  </script><div class="w-full md:h-auto h-[200vh] bg-black p-10 pt-20 flex flex-col md:flex-row justify-around">
-    <div class="w-full h-auto rounded-xl {zoomed?'image-container':' '}" > <img src={map} on:keydown={()=>{toggleZoom}} on:click={()=>toggleZoom}  alt="" style="width: 100%; height: auto;">
+ import {onMount} from 'svelte'
+ let mobile=false;
+ const handleResize=()=>{
+  mobile=(window.innerWidth<1024)
+ }
+  onMount(()=>{
+    handleResize
+    window.addEventListener('resize',handleResize)
+  });
+
+  import Hmap from '$lib/assets/Hmap.png?url';
+  import Vmap from '$lib/assets/Vmap.png?url'
+</script>
+
+
+
+
+
+
+  <div class="w-full h-full bg-black p-10 pt-20 flex flex-col md:flex-row justify-around">
+        <img src={mobile?Vmap:Hmap} alt="" style="width: 100%; height: auto;">
     </div>
-    <div class="w-full h-full"></div>
-    <div class="w-full h-full"></div>
-</div>
-
-
 
   
-  <style>
-    .image-container {
-      overflow: hidden;
-      cursor: pointer;
-      transition: transform 0.5s ease;
-    }
-  
-    .zoomed {
-      transform: scale(2); /* Adjust the scale factor as needed */
-    }
-  </style>
   
